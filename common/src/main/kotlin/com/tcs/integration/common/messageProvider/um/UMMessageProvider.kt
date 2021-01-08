9552a2417077ac92146c85c9681267cbeba61209
sskmscss <sskmscss@yahoo.co.in>
2021-01-08 13:53:41 +0530

    UM subscriber issue fix

--- common/src/main/kotlin/com/tcs/integration/common/messageProvider/um/UMMessageProvider.kt
+++ common/src/main/kotlin/com/tcs/integration/common/messageProvider/um/UMMessageProvider.kt
@@ -1,14 +1,55 @@
 package com.tcs.integration.common.messageProvider.um
 
+import com.pcbsys.nirvana.client.*
 import com.tcs.integration.common.configuration.ConfigProperties
 import com.tcs.integration.common.messageProvider.MessageProvider
+import java.util.concurrent.CopyOnWriteArrayList
 
-class UMMessageProvider (private val configProperties: ConfigProperties): MessageProvider {
+class UMMessageProvider (private val configProperties: ConfigProperties): nEventListener, MessageProvider {
+    private val messages: CopyOnWriteArrayList<String> = CopyOnWriteArrayList<String>()
+    var session: nSession?  = null
+
+    fun getChannel(): nChannel? {
+        val nsa = nSessionAttributes(arrayOf(configProperties.serverUrl))
+        session = nSessionFactory.create(nsa)
+        session?.init()
+
+        val channelAttribute = nChannelAttributes()
+        channelAttribute.setName(configProperties.topic)
+        val channel: nChannel? = session?.findChannel(channelAttribute)
+        channel?.addSubscriber(this, 0)
+
+        return channel
+    }
+
     override fun sendMessage(payload: String) {
-        Config(configProperties).publish(payload)
+        println("Published Message:: " + payload)
+        val props: nEventProperties = nEventProperties()
+        props.put("data", payload)
+        getChannel()?.publish(nConsumeEvent("atag", props, "data".toByteArray()))?.use{}
+    }
+
+    override fun go(event: nConsumeEvent) {
+        try {
+            messages.add(event.properties.getString(String(event.eventData)))
+        } catch (e: nBaseClientException) {
+            // TODO Auto-generated catch block
+            e.printStackTrace()
+        }
     }
 
     override fun subscribeMessage(): String {
-        return Config(configProperties).subscribeMessage()
+        val result = messages.toString()
+        println("SUBSCRIBED MESSAGE:: " + result)
+        messages.clear()
+
+        return result
+    }
+
+    private fun Any.use(function: () -> Unit) {
+        try {
+        } finally {
+            session?.close()
+        }
     }
 }
